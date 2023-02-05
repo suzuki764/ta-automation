7d64e8910dcdaf40733b8d743388799d436fcfde
Kosuke Suzuki <k.suzuki764@gmail.com>
2023-02-06 01:22:36 +0900

    フォルダの中のクラスファイルが削除されないのを修正

--- compile-test.sh
+++ compile-test.sh
@@ -3,5 +3,5 @@ for file in `find . -name "*.java" | sort`; do
     javac $file && echo "\033[32m$file: OK\033[m" || echo "\033[31m $file: Failed\033[m"
 done
 echo "Clean up..."
-rm -f *.class
+find . -name "*.class" -delete;
 echo "Done"
